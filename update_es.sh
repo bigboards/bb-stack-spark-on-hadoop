01472e76b6e1bd218340d6d56b0d125d462cb40e
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-08-10 15:12:09 -0700

    Align lib-add.json and stack.json

--- update_es.sh
+++ update_es.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 curl -v -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d @lib-add.json http://hive.bigboards.io:9200/bigboards-hive/stack/%5Bstack%5Dbigboards%24bb-stack-spark-on-hadoop
